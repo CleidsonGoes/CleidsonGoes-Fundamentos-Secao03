da40815155ea4298c405e01fdad5519ed2791122
CleidsonGoes <jorgegoes4@gmail.com>
2023-03-28 23:56:17 -0300

    questão01

---
+++ secao03_dia01/Exercicio_para_praticar.js
@@ -0,0 +1,14 @@
+//1
+let a = 2;
+let b = 5;
+
+if (a && b) {
+    console.log (a + b);
+    console.log (a - b);
+    console.log (a * b);
+    console.log (a / b);
+    console.log (a % b);
+}
+else {
+    console.log ("digite um valor para operações aritméticas");
+}
