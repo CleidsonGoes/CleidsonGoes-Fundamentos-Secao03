e51dd9d2e6e7bc18c2d01f1d58a1eb711f840e23
CleidsonGoes <jorgegoes4@gmail.com>
2023-03-29 00:01:31 -0300

    questao02_feita

--- secao03_dia01/Exercicio_para_praticar.js
+++ secao03_dia01/Exercicio_para_praticar.js
@@ -12,3 +12,18 @@ if (a && b) {
 else {
     console.log ("digite um valor para operações aritméticas");
 }
+
+//2
+console.log ("\nquestão02");
+let c = 4;
+let d = 4;
+
+if (c > d && c !== d) {
+    console.log ("a variável c é maior que a d");
+}
+else if (c < d && c !== d) {
+    console.log ("a variável d é maior que a c")
+}
+else {
+    console.log ("As variáveis têm valores iguais")
+}
