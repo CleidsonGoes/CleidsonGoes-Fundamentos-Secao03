9efb631893703de12603c6342d21aef30a06e776
CleidsonGoes <jorgegoes4@gmail.com>
2023-03-29 00:28:46 -0300

    questao03_feita

--- secao03_dia01/Exercicio_para_praticar.js
+++ secao03_dia01/Exercicio_para_praticar.js
@@ -27,3 +27,30 @@ else if (c < d && c !== d) {
 else {
     console.log ("As variáveis têm valores iguais")
 }
+
+//3
+console.log ("\nquestão03");
+let e = 10;
+let f = 11;
+let g = 7;
+
+if (e > f && e > g) {
+    console.log ("a variável -e- é maior que as outras");
+}
+else if (f > e && f > g) {
+    console.log ('a variável -f- é maior que as outras');
+}
+else if (g > e && g > f) {
+    console.log ("a variável -g- é maior que as outras");
+}
+else if (e === f && e > g){
+    console.log ("as variáveis -e f- são as maiores");
+}
+else if (e === g && e > f) {
+    console.log ("as variáveis -e g- são as maiores");
+}
+else if (f === g && g > e) {
+    console.log ("as variáveis -f g- são as maiores")
+} else {
+    console.log ("as vaiáveis são todas iguais");
+}
