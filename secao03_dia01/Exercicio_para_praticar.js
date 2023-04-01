8bbf5a4d4cbb12093aca63d7ec0c04a431adfa68
CleidsonGoes <jorgegoes4@gmail.com>
2023-04-01 17:32:47 -0300

    questão 11 feita primeira parte

--- secao03_dia01/Exercicio_para_praticar.js
+++ secao03_dia01/Exercicio_para_praticar.js
@@ -120,19 +120,38 @@ if (valorVenda > custoProduto) {
     console.log ("A empresa ficou no prejuizo de R$ "+ lucro)
 }
 
+console.log ("\nquestão 11");
 // 11 - Utilize if/else para escrever um código que, dado um
 // salário bruto, calcule o salário líquido a ser recebido.
 // Uma pessoa que trabalha de carteira assinada no Brasil 
 // tem descontados de seu salário bruto o INSS e o IR.
 // A notação para um salário de R$1.500,10, por exemplo, deve
 // ser 1500.10. Para as faixas de impostos, use as seguintes referências:
-
+let salarioBruto = 2000;
+let salarioLiquido = [];
+let INSS = "/";
+let IR = 545;
+let aliquota = 0;
 // INSS (Instituto Nacional do Seguro Social)
 // Salário bruto até R$ 1.556,94: alíquota de 8%
 // Salário bruto de R$ 1.556,95 a R$ 2.594,92: alíquota de 9%
 // Salário bruto de R$ 2.594,93 a R$ 5.189,82: alíquota de 11%
 // Salário bruto acima de R$ 5.189,82: alíquota máxima de R$ 570,88
 // IR (Imposto de Renda)
+if (salarioBruto >= 1500.10 && salarioBruto <= 1556.94); {
+    salarioLiquido = salarioBruto - (salarioBruto * 0.08);
+}
+if (salarioBruto >= 1556.95 && salarioBruto <= 2594.92) {
+    salarioLiquido = salarioBruto - (salarioBruto * 0.09);
+}
+else if (salarioBruto >= 2594.93 && salarioBruto <= 5189.82) {
+    salarioLiquido = salarioBruto - (salarioBruto * 0.11);
+}
+else {
+    salarioLiquido = salarioBruto - 570.88
+}
+console.log (salarioLiquido);
+
 // Até R$ 1.903,98: isento de imposto de renda
 // De R$ 1.903,99 a 2.826,65: alíquota de 7,5% e parcela de R$ 142,80 a deduzir do imposto
 // De R$ 2.826,66 a R$ 3.751,05: alíquota de 15% e parcela de R$ 354,80 a deduzir do imposto
